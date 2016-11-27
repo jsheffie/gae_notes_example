008c8b55eedac8acf7a1fc9d85e711949692eaf6
Jeffrey Sheffield <jeff.sheffield@gmail.com>
2016-11-26 18:33:35 -0700

    added initial form for collecting notes (pre-presistant app)

--- main.py
+++ main.py
@@ -23,6 +23,21 @@ class MainHandler(webapp2.RequestHandler):
 			login_url = users.create_login_url(self.request.uri)
 			self.redirect(login_url)
 
+	def post(self):
+		user = users.get_current_user()
+		if user is None:
+			self.errror(401)
+
+		logout_url = users.create_logout_url(self.request.uri)
+		template_context = {
+			'user': user.nickname(),
+			'logout_url': logout_url,
+			'note_title': self.request.get('title'),
+			'note_content': self.request.get('content'),
+		}
+		template = jinja_env.get_template('main.html')
+		self.response.out.write(template.render(template_context))
+
 
 app = webapp2.WSGIApplication([
 	('/', MainHandler)], debug=True)
