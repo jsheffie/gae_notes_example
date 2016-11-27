f4bcdb3cef72ace410e4ed5041f9dbcaeae7523a
Jeffrey Sheffield <jeff.sheffield@gmail.com>
2016-11-26 17:59:02 -0700

    login using google.appengine.api.users

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+from google.appengine.api import users
+
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
-		self.response.write('<h1>Hello World</h1>')
-		self.response.write('<p>I am using GAE, its something new for me</p>')
+		user = users.get_current_user()
+		if user is not None:
+			self.response.write("Hello Notes")
+		else:
+			login_url = users.create_login_url(self.request.uri)
+			self.redirect(login_url)
 
 
 app = webapp2.WSGIApplication([
 	('/', MainHandler)], debug=True)
+
+
+
+
