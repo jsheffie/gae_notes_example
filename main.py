7ecf657d5448a50b7c3a4c15b27f45d48fdbbade
Jeffrey Sheffield <jeff.sheffield@gmail.com>
2016-11-26 18:10:26 -0700

    added jinja2 templating support

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from google.appengine.api import users
+import os
+import jinja2
 
+from google.appengine.api import users
 import webapp2
+
+# initialize the temaplte engine... Before the MainHanlder class def
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
 		user = users.get_current_user()
 		if user is not None:
-			self.response.write("Hello Notes")
+			logout_url = users.create_logout_url(self.request.uri)
+			template_context = {
+				'user': user.nickname(),
+				'logout_url': logout_url,
+			}
+			template = jinja_env.get_template('main.html')
+			self.response.out.write(template.render(template_context))
+			#self.response.write("Hello Notes")
 		else:
 			login_url = users.create_login_url(self.request.uri)
 			self.redirect(login_url)
